ebd98c792b35ed8fffcad28609e9d719405b105b
David <41311088+biagiola@users.noreply.github.com>
2020-09-01 12:11:12 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Post from './components/Post'
 import { makeStyles } from '@material-ui/core/styles';
 import { Modal, Button, Input } from '@material-ui/core';
 
-import { db } from './firebase'
+import { db, auth } from './firebase'
 
 function getModalStyle() {
   const top = 50;
@@ -31,17 +31,35 @@ const useStyles = makeStyles((theme) => ({
 function App() {
   const classes = useStyles();
   const [modalStyle] = useState(getModalStyle)
-
-  // react hook
   const [posts, setPosts] = useState([]);
   const [open, setOpen] = useState(false);
+  const [openSignIn, setOpenSignIn] = useState(false);
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [user, setUser] = useState(null);
 
-  //useEffect runs a piece of code based on a specific condition
-  //the conditions are simply variables
-  //it run one, when page loads, and not again (except if there's a change in the page)
+  useEffect(() => {
+
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      if(authUser) {
+        // user has logged in
+        console.log(authUser)
+        setUser(authUser)
+
+      } else {
+        // user has logged out
+        setUser(null)
+      }
+    })
+
+    return () => {
+      // peform some cleanup actions
+      unsubscribe();
+    }
+  }, [user, username])
+
+  /*useEffect runs a piece of code based on a specific condition. The conditions are simply variables. It run one, when page loads, and not again (except if there's a change in the page)*/
   useEffect(() => {
     db.collection('posts').onSnapshot(snapshot => {
       setPosts(snapshot.docs.map( doc => ({
@@ -52,7 +70,28 @@ function App() {
   }, []);
 
   const signUp = event => {
+    event.preventDefault()
 
+    auth
+    .createUserWithEmailAndPassword(email, password)
+    .then((authUser) => {
+      return authUser.user.updateProfile({
+        displayName: username
+      })
+    }) 
+    .catch((error) => alert(error.message));
+
+    setOpen(false)
+  }
+
+  const signIn = event => {
+    event.preventDefault();
+    
+    auth
+    .signInWithEmailAndPassword(email, password)
+    .catch((error) => alert(error.message))
+
+    setOpenSignIn(false) // close the modal ones you signIn
   }
 
   return (
@@ -94,6 +133,37 @@ function App() {
         </div>
       </Modal>
 
+      <Modal
+        open={openSignIn}
+        onClose={() => setOpenSignIn(false)}>
+
+        <div style={modalStyle} className={classes.paper}>
+          <form className="app__signup">
+            <center>
+              <img 
+                className="app_headerImage"
+                src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" 
+                alt=""
+              />
+            </center>
+            
+            <Input
+              placeholder="email"
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+              placeholder="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button onClick={signIn}>Sign In</Button>
+          </form>
+        </div>
+      </Modal>
+
       <div className="app__header">
         <div className="app__headerImage">
           <img src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" alt=""
@@ -101,7 +171,14 @@ function App() {
         </div>
       </div>
 
-      <Button onClick={() => setOpen(true)}>Sign Up</Button>
+      { user ? 
+        <Button onClick={() => auth.signOut()}>Logout</Button>
+        :
+        <div className="app_loginContai">
+          <Button onClick={() => setOpenSignIn(true)}>Sign In</Button>
+          <Button onClick={() => setOpen(true)}>Sign Up</Button>
+        </div>
+      }
       
       <h1>HELLO CLever Programmers Let's build an Instagram Clone with React!🚀️</h1>
 
